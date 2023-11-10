c965db408b7bd7ff82a0306d7a509f2dff1fac10
thangnt15 <imlight2001@gmail.com>
2023-11-10 19:03:20 +0700

    l

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <div className="profile">
+          <img
+            src="../z4089866810746_8630481cc291b4e10e266aa951a4f935.jpg"
+            alt="Profile Picture"
+          />
+        <h1>Nguyễn Thành Thắng</h1>
+        <p>Web Developer</p>
+        <p>23 years old</p>
+        <p>Ha Noi</p>
+        <p>Make MONEY</p>
+        <p>JS,PHP,ECMA</p>
+      </div>
+
+      
+    </>
+  )
+}
+
+export default App
